434c514356e3cb9b58c383bfac79945a24761caf
Brandon They <theyb5220@gmail.com>
2023-09-12 17:29:55 -0500

    Adding a little parameter handling in the controller servlet

--- S20- JSP And Servlets- Intro/S20- JSP And Servlets- Exercise/src/main/java/s20/JSPAndServlets/Exercise/Controller.java
+++ S20- JSP And Servlets- Intro/S20- JSP And Servlets- Exercise/src/main/java/s20/JSPAndServlets/Exercise/Controller.java
@@ -25,8 +25,13 @@ public class Controller extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//getting the parameter page
+		String param = request.getParameter("page");
+		
+		//conditional forwarding/redirecting based on what page parameter was entered
+		if(param.equals("login")) {
+			getServletContext().getRequestDispatcher("Login.jsp").forward(request, response);
+		} 
 	}
 
 	/**
