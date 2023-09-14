f244e2217b0e915607313c1af0da28f76c2f3634
Brandon They <theyb5220@gmail.com>
2023-09-14 16:56:21 -0500

    Created some properties, firstName and lastName, a constructor and setters and getter for the class

--- S21- JSP And Servlets- Forms And Java Beans/S21- JSP And Servlets- Beans/src/main/java/S21/JSPAndServlets/Beans/User.java
+++ S21- JSP And Servlets- Forms And Java Beans/S21- JSP And Servlets- Beans/src/main/java/S21/JSPAndServlets/Beans/User.java
@@ -1,5 +1,20 @@
 package S21.JSPAndServlets.Beans;
 
 public class User {
-
+	String firstName;
+	String lastName;
+	public User() {
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
