31701b7cd1140900c397ba2cc856d24f3553d38a
Brandon They <theyb5220@gmail.com>
2023-09-20 14:46:18 -0500

    Added some config manipulation

--- S32- Integrating Web Templates/S32- Integrating Web Templates- Revisiting Servlets/src/main/java/S32/IntegratingWebTemplates/RevisitingServlets/demo2.java
+++ S32- Integrating Web Templates/S32- Integrating Web Templates- Revisiting Servlets/src/main/java/S32/IntegratingWebTemplates/RevisitingServlets/demo2.java
@@ -1,10 +1,8 @@
 package S32.IntegratingWebTemplates.RevisitingServlets;
 
-//we can map servlets to a url, when creating a new servlet we can 
-//hit next and see the url mappings
-//since this servlet will serve as our home page, its mapping is just "/"
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,32 +10,30 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Servlet implementation class Home
+ * Servlet implementation class demo2
  */
-public class Home extends HttpServlet {
+
+//The new way of initializing is to instead use initialization parameters in the create
+//new servlet menu
+public class demo2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    String message; //a string variable to be initialized in the init method
+       
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Home() {
+    public demo2() {
         super();
         // TODO Auto-generated constructor stub
     }
 
 	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
-	public void init(ServletConfig config) throws ServletException {
-		message = "Message from init method"; // this is a legacy way of initializing
-	}
-
-	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().println(message);
+		//to access the initialization parameters
+		ServletConfig config = getServletConfig();
+		String message2 = config.getInitParameter("message2");
+		response.getWriter().print(message2);
 	}
 
 	/**
