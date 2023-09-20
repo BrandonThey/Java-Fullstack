9d55406fc72aab0445a0af4728f4efee7c0fe7d6
Brandon They <theyb5220@gmail.com>
2023-09-20 13:49:36 -0500

    Created a new project to revisit servlets and created a home servlet

---
+++ S32- Integrating Web Templates/S32- Integrating Web Templates- Revisiting Servlets/src/main/java/S32/IntegratingWebTemplates/RevisitingServlets/Home.java
@@ -0,0 +1,51 @@
+package S32.IntegratingWebTemplates.RevisitingServlets;
+
+//we can map servlets to a url, when creating a new servlet we can 
+//hit next and see the url mappings
+//since this servlet will serve as our home page, its mapping is just "/"
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Home
+ */
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Home() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- S32- Integrating Web Templates/S32- Integrating Web Templates/src/main/webapp/Demo.jsp
+++ S32- Integrating Web Templates/S32- Integrating Web Templates/src/main/webapp/Demo.jsp
@@ -1,1 +1,15 @@
-<jsp:include page="/include/header.html"></jsp:include>
+<%-- By including the header and footer we can use those html elements in a jsp
+context--%>
+<jsp:include page="/include/header.jsp"></jsp:include>
+<%-- Now just putting an h1 with hello world wont work since the template is 
+containerized so instead we must add a section --%>
+<h1>Hello World</h1>
+
+<%-- using the about container lets us add our h1 hello world and it will show --%>
+<section id="about" class="about">
+      <div class="container">
+		<h1>Hello World</h1>
+      </div>
+</section><!-- End About Section -->
+    
+<jsp:include page="/include/footer.jsp"></jsp:include>
